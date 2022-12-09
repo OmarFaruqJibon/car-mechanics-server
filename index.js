e7b342423aba580abdab651919f093622e634309
unknown <jibon602947@gmail.com>
2022-12-09 17:19:32 +0600

    rainway supported port added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || '0.0.0.0:$PORT';
 
 app.use(cors());
 app.use(express.json());
